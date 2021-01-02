085bd2536e4909bda8420ebdd4283ebdc1df7a36
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-02 22:59:44 +0700

    Fix compileable state
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/TeaBot/TdLibHandler.cpp
+++ src/TeaBot/TdLibHandler.cpp
@@ -1,51 +1,28 @@
 
 #include "TdLibHandler.hpp"
 
+
 namespace TeaBot {
 
+
 /**
- * @param uint32_t   api_id
- * @param const char *api_hash
- * @param const char *data_path
+ * @param uint32_t    api_id
+ * @param const char  *api_hash
+ * @param const char  *data_path
  *
  * Constructor.
  */
 TdLibHandler::TdLibHandler(uint32_t api_id, const char *api_hash, const char *data_path):
     api_id_(api_id)
   , api_hash_(api_hash)
-  , data_path_(data_path)
+  , data_path_(data_path_)
 {
+    td::ClientManager::execute(
+        td_api::make_object<td_api::setLogVerbosityLevel>(1));
 
-}
-
-/**
- * Destructor.
- */
-TdLibHandler::~TdLibHandler()
-{
-}
-
-
-/**
- * @return void
- */
-void loop()
-{
-  while (true) {
-    if (need_restart_) {
-      restart();
-      continue;
-    }
-
-    if (!are_authorized_) {
-      process_response(client_manager_->receive(10));
-      continue;
-    }
-
-    if (!handle_loop()) {
-      break;
-    }
-  }
+    client_manager_ = std::make_unique<td::ClientManager>();
+    client_id_      = client_manager_->create_client_id();
+    send_query(td_api::make_object<td_api::getOption>("version"), {});
 }
 
 
@@ -54,8 +31,32 @@ void loop()
  */
 void TdLibHandler::restart()
 {
-  *this = std::move(TdLibHandler(api_id_, api_hash_, data_path_));
+    client_manager_.reset();
+    *this = TdLibHandler(api_id_, api_hash_, data_path_);
 }
 
+
+/**
+ * @param td_api::object_ptr<td_api::Function>  f
+ * @param std::function<void(Object)>           handler
+ * @return void
+ */
+void TdLibHandler::send_query(td_api::object_ptr<td_api::Function> f, 
+                              std::function<void(Object)> handler)
+{
+    auto query_id = next_query_id();
+    if (handler)
+        handlers_.emplace(query_id, std::move(handler));
+
+    client_manager_->send(client_id_, query_id, std::move(f));
+}
+
+/**
+ * @return uint64_t
+ */
+uint64_t TdLibHandler::next_query_id()
+{
+    return ++current_query_id_;
+}
 
 } /* namespace TeaBot */
--- src/TeaBot/TdLibHandler.hpp
+++ src/TeaBot/TdLibHandler.hpp
@@ -20,27 +20,37 @@
 
 namespace td_api = td::td_api;
 
-using Object = td_api::object_ptr<td_api::Object>;
-
 namespace TeaBot {
 
 class TdLibHandler
 {
 private:
-  uint32_t    api_id_;
-  const char  *api_hash_;
-  const char  *data_path_;
+    uint32_t    api_id_;
+    const char  *api_hash_;
+    const char  *data_path_;
 
-  std::unique_ptr<td::ClientManager> client_manager_;
-  td_api::object_ptr<td_api::AuthorizationState> authorization_state_;
+    bool are_authorized_{false};
+    bool need_restart_{false};
 
-  void restart();
+    int32_t client_id_{0};
+    uint64_t current_query_id_{0};
+    uint64_t authentication_query_id_{0};
 
+    using Object = td_api::object_ptr<td_api::Object>;
+
+    std::unique_ptr<td::ClientManager> client_manager_;
+    td_api::object_ptr<td_api::AuthorizationState> authorization_state_;
+
+    std::map<int64_t, std::string> chat_title_;
+    std::map<uint64_t, std::function<void(Object)>> handlers_;
+    std::map<int32_t, td_api::object_ptr<td_api::user>> users_;
+
+    void restart();
+    uint64_t next_query_id();
 public:
-  TdLibHandler(uint32_t api_id, const char *api_hash, const char *data_path);
-  ~TdLibHandler();
+    TdLibHandler(uint32_t api_id, const char *api_hash, const char *data_path);
 
-  void loop();
+    void send_query(td_api::object_ptr<td_api::Function> f, std::function<void(Object)> handler);
 };
 
 } /* namespace TeaBot */
