ee52bae74293665307bcaaa9d6de6d3e46163297
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-18 23:38:34 +0700

    Clean up
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ src/TeaBot/Modules/ShellExec/Module.hpp
@@ -0,0 +1,24 @@
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license GPL-v3
+ * @package TeaBot::Modules::ShellExec
+ */
+
+#ifndef TEABOT__MODULES__SHELLEXEC__MODULE_HPP
+#define TEABOT__MODULES__SHELLEXEC__MODULE_HPP
+
+#include <TeaBot/TeaBotModule.hpp>
+
+using TeaBot::TeaBotModule;
+
+namespace TeaBot::Modules::ShellExec {
+
+class Module : public TeaBotModule
+{
+
+};
+
+} /* namespace TeaBot::Modules::ShellExec */
+
+#endif
