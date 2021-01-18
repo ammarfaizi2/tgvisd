4745e431791c4e9e6a67a4b8370ed2a9d2f1d883
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-18 14:37:40 +0700

    Clean up TdHandler
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/TeaBot/main.cpp
+++ src/TeaBot/main.cpp
@@ -1,4 +1,10 @@
 
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license GPL-v3
+ * @package TdTeaBot
+ */
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstdint>
