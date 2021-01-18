6c52afefa6374bf8dcc6abcafcebd9910764d78f
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-18 16:46:16 +0700

    Clean up
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/TeaBot/main.cpp
+++ src/TeaBot/main.cpp
@@ -1,4 +1,10 @@
 
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license GPL-v3
+ * @package TeaBot
+ */
+
 #include "TeaBot/TeaBot.hpp"
 
 #include <thread>
