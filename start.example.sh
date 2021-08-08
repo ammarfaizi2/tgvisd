4b84becbae1e636f20aa15a44cc8fbe6436c1c6d
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2021-08-08 23:41:36 +0700

    start.example.sh: add database env variables
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- start.example.sh
+++ start.example.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+export TGVISD_DB_HOST=127.0.0.1
+export TGVISD_DB_PORT=33060
+export TGVISD_DB_USER=username
+export TGVISD_DB_PASS=password
+export TGVISD_DB_NAME=database
 export TGVISD_API_ID=123123
 export TGVISD_API_HASH=aaaaaa
 export TGVISD_DATA_PATH=accounts/6281123123123
