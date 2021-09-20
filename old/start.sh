0e1e35c178b3f521af26e23dc838b5e37886304d
Ammar Faizi <ammarfaizi2@gmail.com>
2021-09-20 22:17:31 +0700

    Full refactor
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ old/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export TGVISD_DB_HOST=127.0.0.1
+export TGVISD_DB_PORT=33060
+export TGVISD_DB_USER=memcpy
+export TGVISD_DB_PASS=858869123qweASDzxc
+export TGVISD_DB_NAME=gw_telegram_new
+export TGVISD_API_ID=2841585
+export TGVISD_API_HASH=fed2843d34aeb38d86cee964243d6ed9
+export TGVISD_DATA_PATH=accounts/6281236285885
+build/tgvisd
